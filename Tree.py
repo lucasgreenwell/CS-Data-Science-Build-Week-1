2a41bd63e5551a3282737b26e13df5c9677823f9
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-07-25 22:23:18 -0600

    sketched out some plans in the comments

---
+++ Tree.py
@@ -0,0 +1,31 @@
+#helper function to split a tree into branches
+
+#helper function to calculate gini impurity
+
+#helper function to connect the data
+
+
+
+#class model that's a decision tree
+
+    #constructor
+
+    #predict method
+
+    #fit method
+
+
+
+
+
+########The Jupyter Notebook Starts Here
+
+#Imports sci-kit-learn's decision tree, data, numpy, my deccision tree, and any helper functions
+
+#helper functions to evaluate algorithms
+
+#Builds the skl decision tree and wires it up to the data
+
+#builds up my decision tree and wires it up to the data
+
+#runs evaluator functions on both trees and shows data
